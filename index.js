a867fcfdba5ba892d6fde36a338f8cd62e0740b4
Mattias Runge <mattias@runge.se>
2017-09-03 13:20:40 +0200

    More refactorizations

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ module.exports = {
     PathHandler: require("./lib/pathhandler").default,
     Readline: require("./lib/readline").default,
     Shell: require("./lib/shell").default,
-    templates: require("./lib/templates").default
+    templates: require("./lib/templates").default,
+    util: require("./lib/util").default,
+    keys: require("./lib/keys").default
 };
